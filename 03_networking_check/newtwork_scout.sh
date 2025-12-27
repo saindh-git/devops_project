459cae21f37c64fe7410067c081516f1af4cafd8
sainadh <sainad.dev@gmail.com>
2025-12-27 08:24:04 -0600

    feat:mastered networking loops and redirection logic

---
+++ 03_networking_check/newtwork_scout.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+Target="127.0.0.1"
+
+for Port in 22 80 443 3000
+do 
+     nc -z -w 1 $Target $Port
+    if [ $? -eq 0 ]; then
+      echo "Port $Port: OPEN (Welcome!)"
+    else 
+       echo "Port $Port: CLOSED(locked)"
+    fi
+done 
